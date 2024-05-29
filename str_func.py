a1ad18a11179d1f1f1ede6de11f3f0acbcc9f62a
Anna-Vl-Green <annagreen.photo@gmail.com>
2024-05-29 22:58:56 +0400

    added new function and docstring

--- str_func.py
+++ str_func.py
@@ -1,2 +1,16 @@
 def str_to_upper(text: str) -> str:
+    """
+    Функция принимает текст и переводит все буквы в верхний регистр
+    :param text: str
+    :return: str
+    """
     return text.upper()
+
+
+def symbols_to_upper(text: str) -> str:
+    """
+    Функция принимает текст и переводит заглавные буквы кождого слова в верхний регистр
+    :param text: str
+    :return: str
+    """
+    return text.title()
